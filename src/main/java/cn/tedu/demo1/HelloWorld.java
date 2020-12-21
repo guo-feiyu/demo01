1df3993dc0ffd394a1c059c7b39b51ef8ab6465c
Robin <guo_yuf@sina.com>
2020-12-21 18:25:24 +0800

    添加了好多Hello Word

--- src/main/java/cn/tedu/demo1/HelloWorld.java
+++ src/main/java/cn/tedu/demo1/HelloWorld.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Word");
-      
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+
     }
 }
